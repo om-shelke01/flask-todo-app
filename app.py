cd3cd8a58ceb48afc05731002f2943bcdc3f15c4
Om Shelke <omshelke2807@gmail.com>
2026-04-11 06:57:37 +0530

    Fix todo ID reference consistency (sno)

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
 class Todo(db.Model):
-    Sno = db.Column(db.Integer, primary_key=True)
+    sno = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
     desc = db.Column(db.String(500), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
@@ -37,18 +37,18 @@ def update(sno):
     if request.method=='POST':
         title = request.form['title']
         desc = request.form['desc']
-        todo = Todo.query.filter_by(Sno=sno).first()
+        todo = Todo.query.filter_by(sno=sno).first()
         todo.title = title
         todo.desc = desc
         db.session.add(todo)
         db.session.commit()
         return redirect("/")
-    todo = Todo.query.filter_by(Sno=sno).first()    
+    todo = Todo.query.filter_by(sno=sno).first()    
     return render_template("update.html", todo=todo)  
 
 @app.route('/delete/<int:sno>')
 def delete(sno):
-    todo = Todo.query.filter_by(Sno=sno).first()    
+    todo = Todo.query.filter_by(sno=sno).first()    
     db.session.delete(todo)
     db.session.commit()
     return redirect('/')  
